3d53abb5242f0ca8e59c288801c33a0bdf89bc3c
arigatopix <teeruch.k@gmail.com>
2020-05-17 13:29:18 +0700

    change at client side

--- complex/client/src/App.js
+++ complex/client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Fib Calculator</h1>
+            <h1 className="App-title">Fibonacci Calculator</h1>
             <div className="flex-container">
               <div>
                 <Link to="/">Home</Link>
