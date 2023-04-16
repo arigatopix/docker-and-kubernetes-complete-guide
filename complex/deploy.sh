afd53d99a0251d4608d35c3dd147df4297912ee0
arigato-mba <teeruch.k@gmail.com>
2023-04-16 18:37:31 +0700

    291. Completed Code For Google Cloud Deployment

--- complex/deploy.sh
+++ complex/deploy.sh
@@ -1,6 +1,6 @@
-docker biuld -t arigatopix/multi-client:latest -t arigatopix/multi-client:$SHA -f ./client/Dockerfile ./client
-docker biuld -t arigatopix/multi-server:latest -t arigatopix/multi-server:$SHA -f ./server/Dockerfile ./server
-docker biuld -t arigatopix/multi-worker:latest -t arigatopix/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t arigatopix/multi-client:latest -t arigatopix/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t arigatopix/multi-server:latest -t arigatopix/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t arigatopix/multi-worker:latest -t arigatopix/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push arigatopix/multi-client:latest
 docker push arigatopix/multi-server:latest
@@ -10,7 +10,7 @@ docker push arigatopix/multi-client:$SHA
 docker push arigatopix/multi-server:$SHA
 docker push arigatopix/multi-worker:$SHA
 
-kubctl apply -f k8s
-kubctl set image deployments/server-deployment server=arigatpix/multi-server:$SHA
-kubctl set image deployments/client-deployment client=arigatpix/multi-client:$SHA
-kubctl set image deployments/worker-deployment worker=arigatpix/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=arigatpix/multi-server:$SHA
+kubectl set image deployments/client-deployment client=arigatpix/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=arigatpix/multi-worker:$SHA
